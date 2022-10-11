812651c0300d405732c5f652cef26b77f9715ad7
mauropascual <riveramauro278@gmail.com>
2022-10-10 21:17:54 -0400

    product offer

---
+++ product_offer/offer.py
@@ -0,0 +1,25 @@
+from database.connect import session
+from datetime import datetime
+from database.schemas import Product
+import math
+
+
+def get_page_of_results(current_page, page_count=5):
+
+    results = session.query(Product).offset((current_page-1)*page_count).limit(page_count).all()#restrict logging to return
+    count_data = session.query(Product).filter(Product.final_time > datetime.now()).count()#count data from database
+
+    if count_data:
+        dictionary = {'results':list(results), 
+                    'current_page':current_page,
+                    'total_pages': math.trunc(count_data/page_count) ,
+                    'total_elements':count_data, 
+                    'element_per_page':page_count}
+    else:
+        dictionary = {'results':None, 
+                    'current_page':None,
+                    'total_pages': None,
+                    'total_elements': None, 
+                    'element_per_page': None}
+                    
+    return dictionary
