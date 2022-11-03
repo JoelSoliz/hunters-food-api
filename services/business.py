96191eb6540d853592b14d70ddc19c2167d13297
saul rosa <rimersaulrosaherrera.com>
2022-11-03 12:50:43 -0300

    obtener datos de business

--- services/business.py
+++ services/business.py
@@ -1,3 +1,5 @@
+import math
+from datetime import datetime
 from sqlalchemy.orm import Session
 
 from data.models.business import Business
@@ -8,6 +10,27 @@ from .utils import generate_id
 class BusinessService:
     def __init__(self, session: Session):
         self.session = session
+
+    def get_Business(self, current_page, page_count=10):
+        results = self.session.query(Business).offset(
+            (current_page-1)*page_count).limit(page_count).all()
+        count_data = self.session.query(Business).filter(
+            Business.final_time > datetime.now()).count()
+
+        if count_data:
+            dictionary = {'results': list(results),
+                          'current_page': current_page,
+                          'total_pages': math.ceil(count_data / page_count),
+                          'total_elements': count_data,
+                          'element_per_page': page_count}
+        else:
+            dictionary = {'results': [],
+                          'current_page': 0,
+                          'total_pages': 0,
+                          'total_elements': 0,
+                          'element_per_page': 0}
+
+        return dictionary
 
     def register_business(self, id_user, business: BusinessCreate, logo):
         id_business = generate_id()
