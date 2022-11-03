334bafdd27abc02e1be5f12fffa00a766306ec5b
Joel Soliz <joelsolizchoque@gmail.com>
2022-11-03 11:41:56 -0400

    Adding get products by business id.

--- api/business.py
+++ api/business.py
@@ -3,14 +3,16 @@ from sqlalchemy.orm import Session
 
 from api.dependencies import get_db_session, get_current_user
 from schemas.business import Business, BusinessCreate
+from schemas.product import ProductPaginated
 from schemas.user import User
 from services.business import BusinessService
+from services.product import ProductService
 
 business_router = APIRouter(prefix='/business')
 
 
 @business_router.post('/register', response_model=Business)
-def register_business(business: BusinessCreate = Depends(), image_logo: UploadFile = File(), session: Session = Depends(get_db_session), user: User=Depends(get_current_user)):
+def register_business(business: BusinessCreate = Depends(), image_logo: UploadFile = File(), session: Session = Depends(get_db_session), user: User = Depends(get_current_user)):
     business_service = BusinessService(session)
     return business_service.register_business(user.id_user, business, image_logo.file.read())
 
@@ -20,5 +22,12 @@ def get_businesss(id: str, session: Session = Depends(get_db_session)):
     business_service = BusinessService(session)
     business = business_service.get_business(id)
     if not business:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"posts id {id} not found. ")  
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"posts id {id} not found. ")
     return business
+
+
+@business_router.get("/{id}/products", response_model=ProductPaginated)
+def get_products_by_business(id: str, current_page: int, session: Session = Depends(get_db_session)):
+    product_service = ProductService(session)
+    return product_service.get_products(current_page, business=id)
