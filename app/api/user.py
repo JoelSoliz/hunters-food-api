4d68f6b6aaf0f493f1ce4a80d3ab057da2bc16b0
Joel Soliz <joelsolizchoque@gmail.com>
2022-11-28 20:26:13 -0400

    Add get favorite business id list endpoint

--- app/api/user.py
+++ app/api/user.py
@@ -5,6 +5,7 @@ from api.dependencies import get_db_session, get_current_user
 from schemas.user import User
 from schemas.business import Business
 from services.business import BusinessService
+from services.favorite import FavoriteService
 
 user_router = APIRouter(prefix='/user')
 
@@ -12,6 +13,7 @@ user_router = APIRouter(prefix='/user')
 @user_router.get("/me", response_model=User, tags=["User"])
 def get_me(user: User = Depends(get_current_user)):
     return user
+
 
 @user_router.get('/business', response_model=Business, tags=["User"])
 def get_business_by_user(session: Session = Depends(get_db_session), user: User = Depends(get_current_user)):
@@ -23,3 +25,11 @@ def get_business_by_user(session: Session = Depends(get_db_session), user: User 
             detail=f"The user {user.id_user} does not have a registered business"
         )
     return get_business
+
+
+@user_router.get('/favorite-business', tags=["User", "Favorite"])
+def get_favorite_businesses_by_user(session: Session = Depends(get_db_session), user: User = Depends(get_current_user)):
+    favorite_service = FavoriteService(session)
+    favorite_businesses = favorite_service.get_favorites_by_user(user.id_user)
+
+    return [business.id_business for business in favorite_businesses]
